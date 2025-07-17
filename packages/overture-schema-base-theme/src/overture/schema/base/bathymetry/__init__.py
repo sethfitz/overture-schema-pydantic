64697cef50040add4427746a43f83154632263b4
Seth Fitzsimmons <sethfitz@amazon.com>
2025-07-17 09:15:49 -0700

    Merge bathymetry-type with base-theme

---
+++ packages/overture-schema-base-theme/src/overture/schema/base/bathymetry/__init__.py
@@ -0,0 +1,5 @@
+"""Bathymetry models for the base theme."""
+
+from .models import Bathymetry
+
+__all__ = ["Bathymetry"]
--- packages/overture-schema-bathymetry-type/src/overture/schema/base/bathymetry/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Bathymetry feature models for Overture Maps base theme."""
-
-from .models import Bathymetry
-
-__all__ = ["Bathymetry"]
